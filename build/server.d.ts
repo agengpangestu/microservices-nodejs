cfbdc9143fbf3d204baf5e09550649a51090c097
agengpangestu <kapalmabur002@gmail.com>
2024-09-19 13:41:18 +0700

    delete

--- build/server.d.ts
+++
@@ -1,2 +0,0 @@
-export { generateToken, verifTokenAdmin, verifTokenAndAuthorizaton, CustomeRequest, JWTPayload, VerifyErrors, verifToken, Secret } from "./jwt";
-//# sourceMappingURL=server.d.ts.map
--- build/server.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.verifToken = exports.verifTokenAndAuthorizaton = exports.verifTokenAdmin = exports.generateToken = void 0;
-var jwt_1 = require("./jwt");
-Object.defineProperty(exports, "generateToken", { enumerable: true, get: function () { return jwt_1.generateToken; } });
-Object.defineProperty(exports, "verifTokenAdmin", { enumerable: true, get: function () { return jwt_1.verifTokenAdmin; } });
-Object.defineProperty(exports, "verifTokenAndAuthorizaton", { enumerable: true, get: function () { return jwt_1.verifTokenAndAuthorizaton; } });
-Object.defineProperty(exports, "verifToken", { enumerable: true, get: function () { return jwt_1.verifToken; } });
